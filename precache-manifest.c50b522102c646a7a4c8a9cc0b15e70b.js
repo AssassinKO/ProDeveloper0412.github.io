b5da5b497ce612cbf468de4a4fa9dd00a1d718b7
ProDeveloper0412 <prodev1992.dev@gmail.com>
2021-10-06 21:22:27 +0300

    Build react app

---
+++ precache-manifest.c50b522102c646a7a4c8a9cc0b15e70b.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4fa93a4caffd78e5eea8620a0e64f86",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4cfc01a5cbfd15f624bb",
+    "url": "/static/css/2.aece854c.chunk.css"
+  },
+  {
+    "revision": "f9827505891eb95134ba",
+    "url": "/static/css/main.9f867508.chunk.css"
+  },
+  {
+    "revision": "4cfc01a5cbfd15f624bb",
+    "url": "/static/js/2.32e1f8cf.chunk.js"
+  },
+  {
+    "revision": "4225fb64c52a283c3b04cbfda343ec4c",
+    "url": "/static/js/2.32e1f8cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9827505891eb95134ba",
+    "url": "/static/js/main.349d80f1.chunk.js"
+  },
+  {
+    "revision": "40281ccf713616220c8d",
+    "url": "/static/js/runtime-main.ec82582c.js"
+  },
+  {
+    "revision": "1470f44e49e511cdfe61aa4a1ab19a6f",
+    "url": "/static/media/disconnected.1470f44e.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "91265a947bb7447a1a6b790e10a5dc59",
+    "url": "/static/media/logo.91265a94.svg"
+  },
+  {
+    "revision": "b0bdf390f04422c69c56f78a4d4f8972",
+    "url": "/static/media/tour.b0bdf390.svg"
+  },
+  {
+    "revision": "cfb49f55eb8da55ee5e66a7587b12849",
+    "url": "/static/media/unselected-chat.cfb49f55.svg"
+  }
+]);
